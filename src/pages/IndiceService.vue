8d49ee0133623d8613b489e83e831a5c8b897e49
juanyonda <juan.yonda@misena.edu.co>
2023-06-27 17:01:08 -0500

    diseños

--- src/pages/IndiceService.vue
+++ src/pages/IndiceService.vue
@@ -1,13 +1,16 @@
 <template>
-  <section class="col-12 mt-6 d-flex">
-    <div class="w-50">
+  <!-- conocenos -->
+  <section class="col-12 mt-6 d-flex conocenos-section flex-wrap" image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg">
+    <v-col cols="12" md="7" lg="6" align-self="end" class="pa-0">
+      <div class="d-flex align-end">
       <v-img
-        max-height="850"
-        max-width="680"
+        class="w-100"
         src="./src/assets/quienes_somos.png"
       ></v-img>
     </div>
-    <div class="w-50">
+    </v-col>
+
+    <div class="w-50 pa-2">
       <v-card
         class="mt-5 bg-primary"
         width="500"
@@ -47,8 +50,36 @@
           Reprehenderit porro consectetur enim iusto placeat quod.
         </v-card-text>
       </v-card>
+
+      <v-card
+        class="mt-5 bg-primary"
+        width="500"
+        prepend-icon="mdi-flag"
+        variant="tonal"
+      >
+        <template v-slot:title>Estrategia en Materia de responsabilidad social</template>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Reprehenderit porro consectetur enim iusto placeat quod.
+        </v-card-text>
+      </v-card>
+      <v-card
+        class="mt-4 bg-primary"
+        width="500"
+        prepend-icon="mdi-sign-direction"
+        variant="tonal"
+      >
+        <template v-slot:title> Vision </template>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Reprehenderit porro consectetur enim iusto placeat quod.
+        </v-card-text>
+      </v-card>
     </div>
   </section>
+  <!-- Modulos gratuitos -->
   <section class="col-12">
     <p class="text-lg-h4 bg-secondary text-center pt-3 pb-3">
       Modulo Gratuito <v-icon>mdi-wallet-giftcard</v-icon>
@@ -199,3 +230,13 @@ export default {
   }),
 };
 </script>
+
+<style>
+@media (min-width: 1800px) {
+  /* … */
+  .conocenos-section{
+   height: 77vh; 
+}
+}
+
+</style>
