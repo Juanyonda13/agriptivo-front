24975f39cd9a0f35ca07a0550487c53d35b302aa
Juanyonda13 <juan.yonda@misena.edu.co>
2023-07-10 02:13:03 -0500

    git push

--- src/pages/finca/Gestionar.vue
+++ src/pages/finca/Gestionar.vue
@@ -1,50 +1,52 @@
 <template>
-    <div>
-      <!-- <p>{{ fincas }}</p> -->
-      <v-data-table class="elevation-1" :headers="headers" :items="fincas">
-        <!-- Contenido adicional del v-data-table -->
-      </v-data-table>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        headers: [
-          {
-            text: "Finca",
-            align: "start",
-            value: "name_finca",
-          },
-        ],
-        fincas: [],
-      };
+  <div>
+    <p>{{ fincas }}</p>
+    <v-data-table 
+        class="elevation-1" 
+        :headers="headers" 
+        :items="fincas"
+        item-value="name_finca"
+    >
+      <!-- Contenido adicional del v-data-table -->
+    </v-data-table>
+  </div>
+</template>
+<script>
+export default {
+  data:()=>({
+      headers: [
+      {
+          title: 'Dessert (100g serving)',
+          align: 'start',
+          key: 'name_finca',
+        },
+      ],
+      fincas: [],
+  }),
+  computed: {
+    user() {
+      return this.$store.getters["auth/currentUser"];
     },
-    computed: {
-      user() {
-        return this.$store.getters["auth/currentUser"];
-      },
+  },
+  mounted() {
+    this.getUserInfo();
+    this.getFincas();
+  },
+  methods: {
+    async getUserInfo() {
+      await this.$store.dispatch("auth/userInfo");
     },
-    mounted() {
-      this.getUserInfo();
-      this.getFincas();
+
+    async getFincas() {
+      try {
+        await this.$store.dispatch("finca/list", this.user._id);
+        const res = this.$store.getters["finca/fincas"];
+        this.fincas = Array.isArray(res) ? res : [res];
+      } catch (error) {
+        console.error(error);
+      }
     },
-    methods: {
-      async getUserInfo() {
-        await this.$store.dispatch("auth/userInfo");
-      },
-  
-      async getFincas() {
-        try {
-          await this.$store.dispatch("finca/list", this.user._id);
-          const res = this.$store.getters["finca/fincas"];
-          this.fincas = Array.isArray(res) ? res : [res];
-        } catch (error) {
-          console.error(error);
-        }
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
   
